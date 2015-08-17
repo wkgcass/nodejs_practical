51e405ac074962a712b0eba8b17a73fe1090a424
wkgcass <wkgcass@hotmail.com>
2015-08-18 00:03:44 +0800

    final

--- Cassite/routes/index.js
+++ Cassite/routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render("index");
+router.get('/', function (req, res) {
+    res.render("index", {
+        "title": "Cass的个人主页"
+    });
 });
 
 module.exports = router;
